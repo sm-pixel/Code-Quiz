ffe518a9e95dba09e3fb0e55652e5ed73512cb65
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-05-04 20:07:10 -0400

    Added bootstrap, formated htmls to match requierments, psuedo code on js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Start quiz (alert and button)
+
+// Questions (5-10) & Answers (4 each question)
+
+// Variables for score and timer
+
+// Add 10-20 points for each right answer
+
+// Remove 10-15 seconds for each wrong answer
+
+// Loop through Questions
---
+++ score.js
@@ -0,0 +1,7 @@
+// Stop timer or answers all questions to end game
+
+// Store Name and Score to display for Highscore
+
+// Clears Name and Highscore
+
+// Reset game
